b512a0d13365310293ead12cf587f47b85e9f269
gknsntrk90 <gokhansenturk1990@gmail.com>
2024-08-19 22:34:01 +0300

    first commit

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -4,12 +4,14 @@ const Select = ({ label, options, name, handleChange }) => {
             <label>{label}</label>
             <select name={name} onChange={handleChange}>
                 <option value="">Seçiniz</option>
-                {options.map((i) => (
+                {options?options.map((i) => (
                     <option key={i} value={i}>
                         {i}
                     </option>
-                ))}
+                )):''}
             </select>
         </div>
     )
 }
+
+export default Select
